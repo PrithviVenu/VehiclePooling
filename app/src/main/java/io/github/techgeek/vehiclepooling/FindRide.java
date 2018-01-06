5935fb1892133fc24b58b83f9992d4dd754acd37
Prithvi Venu <prithviv30@gmail.com>
2018-01-07 03:03:38 +0530

    Structure definition

---
+++ app/src/main/java/io/github/techgeek/vehiclepooling/FindRide.java
@@ -0,0 +1,33 @@
+package io.github.techgeek.vehiclepooling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FindRide extends AppCompatActivity {
+    int quantity=1;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_ride);
+    }
+    public void increment(View view) {
+
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+    public void decrement(View view) {
+        if (quantity == 0) {
+            return;
+        }
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+    private void displayQuantity(int numberOfCoffees) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.textView3);
+        quantityTextView.setText("" + numberOfCoffees);
+    }
+}
--- app/src/main/java/io/github/techgeek/vehiclepooling/MainActivity.java
+++ app/src/main/java/io/github/techgeek/vehiclepooling/MainActivity.java
@@ -1,7 +1,9 @@
 package io.github.techgeek.vehiclepooling;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    protected void offer(View v) {
+        Intent myIntent = new Intent(this, OfferRide.class);
+        startActivity(myIntent);
+
+    }
+    protected void find(View v) {
+        Intent myIntent = new Intent(this, FindRide.class);
+        startActivity(myIntent);
     }
 }
