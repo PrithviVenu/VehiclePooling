0a099a63563cd2d524190c201c275ce073ccebb8
Prithvi Venu <prithviv30@gmail.com>
2018-01-06 21:24:39 +0530

    Complete login

--- app/src/main/java/io/github/techgeek/vehiclepooling/welcome.java
+++ app/src/main/java/io/github/techgeek/vehiclepooling/welcome.java
@@ -1,7 +1,11 @@
 package io.github.techgeek.vehiclepooling;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class welcome extends AppCompatActivity {
 
@@ -10,4 +14,11 @@ public class welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+    protected void login(View view) {
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
+
+
+    }
+
 }
